cd04c90ebba8cd1d54bd9c4f38e9434e98d57e1f
DavidV01 <david.vodak@email.cz>
2022-07-04 10:49:59 +0200

    features

---
+++ Chosed_stone.py
@@ -0,0 +1,19 @@
+#from ast import Break
+#from Stone import stone
+#from C_M import center_mouse
+from updt import screen_update
+from Is_st import is_stone
+#from Is_st import is_true
+import pygame as pg
+#from Plr import Player   #ošetřit, že jen ten hráč, co hraje hýbá se svými kameny
+
+def Chosed(mouse_p,ws,bs,bg,scr,ch_stone,Pl_now):
+    chosed=is_stone(ws,bs,mouse_p,ch_stone)       #vrátí, zda je ten kámen na stejný pozici, jako myš 
+    
+    if (chosed.get_color())==(Pl_now.get_color()):     #zda má vybraný kámen stejnou bravu jako hráč
+            ch_stone.append(chosed)                             #přidej vybranej stone do seznamu, pro kontrolu
+            pg.draw.circle(bg, 'red', mouse_p,40)          
+            screen_update(scr,bg)
+                    #print(ch_stone[0]) 
+
+    return ch_stone
--- move_d.py
+++ move_d.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from ast import Break
 from Stone import stone
 from C_M import center_mouse
@@ -35,3 +36,28 @@ def move_stone(mouse_p,ws,bs,bg,scr,tile_s,ch_stone,new_rct_pos):     #ch_stone 
             screen_update(scr,bg)   
         
     return ch_stone,new_rct_pos
+=======
+#from ast import Break
+#from Stone import stone
+from C_M import center_mouse
+from updt import screen_update
+#from Is_st import is_stone
+#from Is_st import is_true
+import pygame as pg
+#from Plr import Player   
+
+
+def move_stone(mouse_p,bg,scr,tile_s,ch_stone,new_rct_pos,bol,next_player): 
+
+        if (ch_stone!=[])&(bol==False):          #pokud už mám vybraný kámen a hodlám udělat tah -> kliknu na pole bez kamene
+            rect = (new_rct_pos[0],new_rct_pos[1], tile_s, tile_s)    #TADY NĚKDE OŠETŘIT, JAK MOC SE MOHU HÝBAT -> FCE NAPŘ.: CHECK_MOVE!!!!!
+            pg.draw.rect(bg, 'black', rect)
+            pg.draw.circle(bg, ch_stone[0].get_color(), mouse_p,40)                                 
+            ch_stone[0].center(mouse_p)   #chosed.center(mouse_p)
+            screen_update(scr,bg)
+            next_player=True 
+              
+        return next_player   
+        
+    
+>>>>>>> Stashed changes
