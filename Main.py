0a2451e1a7dab8292bf6f9b08473a800fce0e1a8
DavidV01 <david.vodak@email.cz>
2022-07-15 08:41:24 +0200

    dama - up

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 #import csv
 import pygame as pg
 import itertools
-from l_s import load_stones
+from l_s import load_stones,load_queens
 from Pr_s import print_stones
 from Pl_s import place_stones
 from Stone import stone
@@ -25,7 +25,11 @@ from Game_modes import mode_pvp
 def main():
     #názvy pro kameny
     w_stones = ["W1","W2","W3","W4","W5","W6","W7","W8","W9","W10","W11","W12"]
-    b_stones = ["B1","B2","B3","B4","B5","B6","B7","B8","B9","B10","B11","B12"]     
+    w_queens = ["DW1","DW2","DW3","DW4","DW5","DW6"]      #dáma je ze 2 kamenů, max 6 dam, v programu budu počítat zaměnu jednoho kamene za dámu -> konec jakmile len(vyhozených) bude 12
+
+    b_stones = ["B1","B2","B3","B4","B5","B6","B7","B8","B9","B10","B11","B12"]   
+    b_queens = ["DB1","DB2","DB3","DB4","DB5","DB6"]
+
     Players=[]
     thrown_aw_b=[]
     thrown_aw_w=[]    
@@ -52,11 +56,12 @@ def main():
     
     #načtu kameny jako entity se svým jménem a základní pozicí (0,0)
     load_stones(w_stones,b_stones)
-    
+
+    load_queens(w_queens,b_queens)   
 
     
     #kontrola položení kamenů, zda je v csv vše správně atd.
-    if place_stones(w_stones,b_stones)==True:
+    if place_stones(w_stones,b_stones,w_queens,b_queens)==True:
         
         #vyhození kamenů, co nejsou ve hře
         thrown_aw_w=away(w_stones,thrown_aw_w)
@@ -68,12 +73,14 @@ def main():
         #filtrování kamenů tak, aby zbyl jen ty, co jsou ve hře
         #pro ně udělat stromy
         w_stones=filt(w_stones,thrown_aw_w)
-        b_stones=filt(b_stones,thrown_aw_b)        
+        b_stones=filt(b_stones,thrown_aw_b) 
+
+              
 
 
         #načtení PVP modu
         if player_question=="PVP":
-            mode_pvp(w_stones,b_stones,Players,thrown_aw_b,thrown_aw_w)
+            mode_pvp(w_stones,b_stones,Players,thrown_aw_b,thrown_aw_w,w_queens,b_queens)
         else:
             print("konec hry")
           
