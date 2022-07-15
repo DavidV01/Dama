25742d77c982aa02798e79ac389dda1ed1e7ac5d
DavidV01 <david.vodak@email.cz>
2022-07-15 08:44:07 +0200

    dama-up

--- Stone.py
+++ Stone.py
@@ -1,6 +1,17 @@
-class stone:
+class Figure:
+    def __innit__(self,name):
+        self._name=name
+
+    def get_name(self):
+        return self._name
+
+
+
+
+#jako kruh
+class stone(Figure):
     def __init__(self,name,color, center, radius):
-        self._name = name
+        super().__innit__(name)
         self._center = center
         self._radius = radius        
         self._color=color    
@@ -13,13 +24,29 @@ class stone:
         print(str(self._color))
         return "\n"
 
-    def get_name(self):
-        return self._name
+    
    
 
     def center(self,center):
         self._center=center
     
+    def get_center(self):
+        return(list(self._center))
+
+    def get_color(self):
+        return self._color
+
+#jako elipsa
+class Queen(Figure):
+    def __init__(self,name,color, center, radius):
+        super().__innit__(name)
+        self._center = center
+        self._radius = radius        
+        self._color=color
+    
+    def center(self,center):
+        self._center=center
+
     def get_center(self):
         return(list(self._center))
 
